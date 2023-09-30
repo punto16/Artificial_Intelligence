835c2f235a676f87607ec6a665f5aa74d886dde3
Luis González <lluis.gora@gmail.com>
2023-09-30 23:01:21 +0200

    flocking exercise first commit

---
+++ Artificial_Intelligence/Artificial Intelligence/Assets/scripts/FlockManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockManager : MonoBehaviour
+{
+    public GameObject FishPrefab;
+    public int numFish;
+    public Vector3 swimLimits;
+    public bool bounded;
+    public bool randomize;
+    public bool followLider;
+    public GameObject lider;
+    public float minSpeed;
+    public float maxSpeed;
+    public float distance;
+    public float rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
