578eaea89dc0801c8b6b9e833f6172f6b9b99725
Luis González <luis.gonzalez.ramirez@estudiantat.upc.edu>
2023-10-19 10:27:32 +0200

    xd tonto manel

--- Artificial_Intelligence/Artificial Intelligence/Assets/scripts/PerceptionEvent.cs
+++ Artificial_Intelligence/Artificial Intelligence/Assets/scripts/PerceptionEvent.cs
@@ -6,28 +6,25 @@ public class PerceptionEvent : MonoBehaviour
 {
     // Start is called before the first frame update
     [Header("Group options")]
-    public uint numZombies = 5;
-    public float spawnSpread = 2.0f;
+    public int numZombies = 5;
+    public Vector3 spawnLimits = new Vector3(5, 5, 5);
     public GameObject[] zombies;
     public GameObject zombiePrefab;
 
 
-    private AIVision aivision;
+    //private AIVision aivision;
 
     void Start()
     {
         zombies = new GameObject[numZombies];
-
         for (int i = 0; i < numZombies; ++i)
         {
+            Vector3 randVec = new Vector3(UnityEngine.Random.Range(-spawnLimits.x, spawnLimits.x), UnityEngine.Random.Range(-spawnLimits.y, spawnLimits.y), UnityEngine.Random.Range(-spawnLimits.z, spawnLimits.z));
+            Vector3 pos = this.transform.position + randVec; // random position
 
-            Vector3 position = this.transform.position + Random.insideUnitSphere * spawnSpread;
-            position.y = this.transform.position.y;
-            Vector3 direction = Random.insideUnitSphere; // random vector direction
-            direction.y = 0;
-            zombies[i] = (GameObject)Instantiate(zombiePrefab, position, Quaternion.LookRotation(direction), this.transform);
+            zombies[i] = (GameObject)Instantiate(zombiePrefab, pos, Quaternion.identity);
+            //zombies[i].GetComponent<Flock>().fManager = this;
         }
-
     }
 
     // Update is called once per frame
